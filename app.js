be3e6f5886767a2ec685974eb78275073d15a4a5
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2021-04-07 20:44:36 -0400

    changed port

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
 })
 
 
